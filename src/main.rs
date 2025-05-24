2532cc355f253ebc8e0921cee2514ff005bfe0dd
Lewis <915262557@qq.com>
2025-05-24 17:47:13 +0800

    second commit

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,58 @@ enum GameMode{
     Playing,
     End,
 }
+
+const SCREEN_WIDTH : i32 =80;
+const SCREEN_HEIGHT : i32 =50;
+const FRAME_DURATION: f32=75.0;
+
+struct Player {
+    x : i32,
+    y : i32,
+    velocity :f32,
+}
+
+impl Player {
+    fn new() -> Self{
+        Player { x:25, y: 25, velocity: 0.0 }
+    }
+    fn render(&mut self, ctx : &mut BTerm){
+    ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+    fn gravity_and_move(&mut self){
+        if self.velocity <2.0{
+            self.velocity+=0.2;
+        }
+        self.y=self.y  +  self.velocity as i32;
+        self.x+=1;
+
+        if self.y<0 {
+            self.y=0;
+        }
+    }
+
+    fn flap(&mut self){
+        self.velocity = -2.0;
+    }
+}
+
+
 struct State {
+    player :Player,
+    frame_time :f32,
     mode :GameMode,
 }
 impl State{
-    fn new() -> Self { 
+    fn new() -> Self {
         State{
+            player: Player::new(),
+            frame_time :0.0,
             mode : GameMode::Menu,
         }
     }
     fn restart(&mut self){
+        self.frame_time=0.0;
+        self.player=Player::new();
         self.mode=GameMode::Playing;
     }
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -27,16 +69,37 @@ impl State{
             match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting=true,
-                _ => {}                
+                _ => {}
             }
         }
     }
-    
+
     fn play(&mut self, ctx: &mut BTerm) {
-        self.mode=GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if(self.frame_time>FRAME_DURATION){
+            /*
+           Frame_Duration刷新一次
+             */
+            self.frame_time=0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) =ctx.key{
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0,0,"Press Space to Flap");
+
+        if(self.player.y>SCREEN_HEIGHT){
+            self.mode=GameMode::End;
+        }
+
+
     }
 
-    
+
     fn dead(&mut self, ctx: &mut BTerm) {
                 ctx.cls();
         ctx.print_centered(5, "You are dead!");
@@ -47,7 +110,7 @@ impl State{
             match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting=true,
-                _ => {}                
+                _ => {}
             }
         }
     }
